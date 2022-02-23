d81cb0da809b8991b7827e5f5c1d194e2b141673
Fareal Ahmed <farealahmed@gmail.com>
2022-02-23 13:28:49 +1100

    email post and only name updated and delete done

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var url = "mongodb://localhost:27017/studentinfodb";
 
 
 //insert data
-const savehandler = (req, res) => {
+const savedhandler = (req, res) => {
   MongoClient.connect(url, function (err, db) {
     if (err) throw err;
     var dbo = db.db("studentinfodb");
@@ -57,7 +57,7 @@ const getalldatahandler = (req, res) => {
 };
 
 //find data by posting data on postman body and retrieve
-const datatosearchonehandler = (req, res) => {
+const searchonedatabypostmanhandler = (req, res) => {
   MongoClient.connect(url, function (err, db) {
     if (err) throw err;
     var dbo = db.db("studentinfodb");
@@ -73,23 +73,24 @@ const datatosearchonehandler = (req, res) => {
 
 
 
-//update data
-
-const updatedatahandler = (req, res) => {
-
-  MongoClient.connect(url, function (err, db) {
-    if (err) throw err;
-    var dbo = db.db("studentinfodb");
-    var myobj = req.body;
-    // var newvalues = { $set: {studentName: myobj.studentName, studentClass: myobj.studentClass,
+//data was set from postman
+ // var newvalues = { $set: {studentName: myobj.studentName, studentClass: myobj.studentClass,
     //     studentSession: myobj.studentSession, studentEmail: myobj.studentEmail,
     //     fatherName: myobj.fatherName, fatherEmail: myobj.fatherEmail, 
     //     fatherPhone: myobj.fatherPhone, motherName: myobj.motherName,
     //     motherEmail: myobj.motherEmail, motherPhone: myobj.motherPhone,
     //     address: myobj.address} 
     // };
+
+//update data
+const updatedatahandler = (req, res) => {
+  MongoClient.connect(url, function (err, db) {
+    if (err) throw err;
+    var dbo = db.db("studentinfodb");
+    var myobj = {studentEmail: req.body.studentEmail};
+    var updatedOne= {$set: {studentName: req.body.studentName}};
     console.log(myobj);
-    dbo.collection("studentInformation").updateOne({ _id: new ObjectId(myobj._id) }, { $set: { studentEmail: "fat@gmail.com" } }, function (err, result) {
+    dbo.collection("studentInformation").updateOne(myobj, updatedOne, function (err, result) {
       if (err) throw err;
       console.log("Updated", result);
       db.close();
@@ -103,27 +104,11 @@ const updatedatahandler = (req, res) => {
 
 //delete data
 
-const getposthandler = (req, res) => {
-  deletedData = "";
+const deletedatahandler = (req, res) => {
   MongoClient.connect(url, function (err, db) {
     if (err) throw err;
     var dbo = db.db("studentinfodb");
-    var myobj = req.body;
-    dbo.collection("studentInformation").findOne({ studentEmail: myobj.studentEmail }, {}, function (err, result) {
-      if (err) throw err;
-      console.log("Got all result", result);
-      db.close();
-      res.send(result);
-      deletedData = result;
-    });
-  });
-};
-
-const getdeletehandler = (req, res) => {
-  MongoClient.connect(url, function (err, db) {
-    if (err) throw err;
-    var dbo = db.db("studentinfodb");
-    var myquery = { studentEmail: deletedData.studentEmail };
+    var myquery = { studentEmail: req.body.studentEmail};
     dbo.collection("studentInformation").deleteOne(myquery, function (err, obj) {
       if (err) throw err;
       console.log("1 document deleted");
@@ -137,11 +122,12 @@ const getdeletehandler = (req, res) => {
 
 
 //api handlers
-app.post('/save', savehandler);
+app.post('/save', savedhandler);
 app.get('/findone', findfirstdatahandler);
 app.get('/getalldata', getalldatahandler);
-app.post('/search', datatosearchonehandler);
+app.post('/search', searchonedatabypostmanhandler);
 app.put('/update', updatedatahandler);
+app.delete('/delete', deletedatahandler);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
